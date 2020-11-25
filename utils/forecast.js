eaa9145c04fb0cead0ee34cb519d47039dfe295f
przemskydev <przemekgithub@o2.pl>
2020-11-25 12:53:18 +0100

    refactor of const url name and shorthand url in request fn

--- utils/forecast.js
+++ utils/forecast.js
@@ -5,7 +5,7 @@ const forecast = (lat, long, callback) => {
 
   const url = `http://api.weatherstack.com/current?access_key=${key}&query=${lat},${long}`
 
-  request({ url: url, json: true }, (error, { body }) => {
+  request({url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connection to weather server', undefined)
     } else if (body.error) {
--- utils/geocode.js
+++ utils/geocode.js
@@ -2,9 +2,9 @@ const request = require('request')
 const { gelocation } = require('../key')
 
 const geocode = (address, callback) => {
-  const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${gelocation}`
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${gelocation}`
 
-  request({ url: geoUrl, json: true }, (error, { body }) => {
+  request({ url, json: true }, (error, { body }) => {
 
     if (error) {
       callback('Unable to connection to geolocation server', undefined)
